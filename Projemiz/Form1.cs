59c2006781fb3ea709c67f8f5809fe87313ba676
Talha Aslan <talhabesiktas1@gmail.com>
2023-10-23 16:49:36 +0300

    güncel hal

--- Projemiz/Form1.cs
+++ Projemiz/Form1.cs
@@ -1,25 +1,69 @@
 namespace Projemiz
 {
-	public partial class FormAnaSyf : Form
-	{
-		public FormAnaSyf()
-		{
-			InitializeComponent();
-		}
+    public partial class FormAnaSyf : Form
+    {
+        public FormAnaSyf()
+        {
+            InitializeComponent();
+        }
 
-		private void button1_Click(object sender, EventArgs e)
-		{
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hamming form1 = new Hamming();
+            form1.Show();
+            this.Hide();
+        }
 
-		}
+        private void label1_Click(object sender, EventArgs e)
+        {
 
-		private void label1_Click(object sender, EventArgs e)
-		{
+        }
 
-		}
+        private void FormAnaSyf_Load(object sender, EventArgs e)
+        {
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+        }
 
-		private void FormAnaSyf_Load(object sender, EventArgs e)
-		{
-			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-		}
-	}
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WordToHamming form2 = new WordToHamming();
+            form2.Show();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            LRCcs form3 = new LRCcs();
+            form3.Show();
+            this.Hide();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            CRC form4 = new CRC();
+            form4.Show();
+            this.Hide();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            FEC form5 = new FEC();
+            form5.Show();
+            this.Hide();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            BCH form6 = new BCH();
+            form6.Show();
+            this.Hide();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Reed_Solomon form7 = new Reed_Solomon();
+            form7.Show();
+            this.Hide();
+        }
+    }
 }
---
+++ Projemiz/Hamming.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projemiz
+{
+    public partial class Hamming : Form
+    {
+        public Hamming()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // PDF dosyasının yolunu belirtin
+            string pdfDosyaYolu = "E:\\CVler\\TalhaAslan-İngilizceCV (2).pdf"; // Gerçek dosya yolunu belirtin
+
+            // Dosyanın varlığını kontrol etmek isterseniz:
+            if (File.Exists(pdfDosyaYolu))
+            {
+                // Dosyayı MemoryStream'e yükle
+                byte[] pdfBytes = File.ReadAllBytes(pdfDosyaYolu);
+                MemoryStream memoryStream = new MemoryStream(pdfBytes);
+
+                // SaveFileDialog ile kullanıcıya nereye kaydetmek istediğini sorma
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "PDF Dosyaları (*.pdf)|*.pdf";
+                saveFileDialog.FileName = "YeniDosyaAdi.pdf"; // İndirilen dosyanın adını belirleyin
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Dosyayı kullanıcının belirttiği yere kaydet
+                    File.WriteAllBytes(saveFileDialog.FileName, memoryStream.ToArray());
+                    MessageBox.Show("PDF İndirme Başarılı!", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Belirtilen PDF dosyası bulunamadı.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
