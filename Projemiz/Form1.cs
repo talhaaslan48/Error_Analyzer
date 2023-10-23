ab5a8952e000909e66e9112c6d956d87ab71dbf8
Talha Aslan <talhabesiktas1@gmail.com>
2023-10-23 17:37:40 +0300

    mustafa

--- Projemiz/Form1.cs
+++ Projemiz/Form1.cs
@@ -7,68 +7,67 @@ namespace Projemiz
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Hamming form1 = new Hamming();
-            form1.Show();
-            this.Hide();
-        }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
+     
 
         private void FormAnaSyf_Load(object sender, EventArgs e)
         {
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
         }
 
-        private void button2_Click(object sender, EventArgs e)
+
+        private void FormAnaSyf_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Hamming form1 = new Hamming();
+            form1.Show();
+            this.Hide();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
         {
             WordToHamming form2 = new WordToHamming();
             form2.Show();
             this.Hide();
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void button3_Click_1(object sender, EventArgs e)
         {
             LRCcs form3 = new LRCcs();
             form3.Show();
             this.Hide();
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        private void button4_Click_1(object sender, EventArgs e)
         {
             CRC form4 = new CRC();
             form4.Show();
             this.Hide();
         }
 
-        private void button5_Click(object sender, EventArgs e)
+        private void button5_Click_1(object sender, EventArgs e)
         {
             FEC form5 = new FEC();
             form5.Show();
             this.Hide();
         }
 
-        private void button6_Click(object sender, EventArgs e)
+        private void button6_Click_1(object sender, EventArgs e)
         {
             BCH form6 = new BCH();
             form6.Show();
             this.Hide();
         }
 
-        private void button7_Click(object sender, EventArgs e)
+        private void button7_Click_1(object sender, EventArgs e)
         {
             Reed_Solomon form7 = new Reed_Solomon();
             form7.Show();
-            this.Hide();  //yarenyazýyor
+            this.Hide();
         }
-
-		private void FormAnaSyf_Load_1(object sender, EventArgs e)
-		{
-
-		}
-	}
+    }
 }
