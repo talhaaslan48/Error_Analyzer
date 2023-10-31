3817e755a351b497df71e01a76cfd60718b021e1
Talha Aslan <talhabesiktas1@gmail.com>
2023-10-31 16:51:20 +0300

    crc tamamlandı

--- Projemiz/CRC.Designer.cs
+++ Projemiz/CRC.Designer.cs
@@ -32,6 +32,18 @@
             label2 = new Label();
             button1 = new Button();
             label1 = new Label();
+            dividendTextBox = new TextBox();
+            divisorTextBox = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            button2 = new Button();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            button3 = new Button();
             SuspendLayout();
             // 
             // label2
@@ -53,6 +65,7 @@
             button1.TabIndex = 4;
             button1.Text = "PDF İndir";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
             // 
             // label1
             // 
@@ -63,11 +76,131 @@
             label1.TabIndex = 5;
             label1.Text = resources.GetString("label1.Text");
             // 
+            // dividendTextBox
+            // 
+            dividendTextBox.Location = new Point(415, 422);
+            dividendTextBox.Name = "dividendTextBox";
+            dividendTextBox.Size = new Size(152, 27);
+            dividendTextBox.TabIndex = 6;
+            dividendTextBox.KeyPress += CRCcs_KeyPress;
+            // 
+            // divisorTextBox
+            // 
+            divisorTextBox.Location = new Point(415, 525);
+            divisorTextBox.Name = "divisorTextBox";
+            divisorTextBox.Size = new Size(152, 27);
+            divisorTextBox.TabIndex = 7;
+            divisorTextBox.KeyPress += CRCcs_KeyPress;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(286, 422);
+            label3.Name = "label3";
+            label3.Size = new Size(72, 20);
+            label3.TabIndex = 8;
+            label3.Text = "Dividend:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(286, 525);
+            label4.Name = "label4";
+            label4.Size = new Size(55, 20);
+            label4.TabIndex = 9;
+            label4.Text = "Divisor";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(714, 466);
+            button2.Name = "button2";
+            button2.Size = new Size(136, 45);
+            button2.TabIndex = 10;
+            button2.Text = "Hesapla";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click_2;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(1056, 373);
+            label5.Name = "label5";
+            label5.Size = new Size(69, 20);
+            label5.TabIndex = 11;
+            label5.Text = "CRC Biti :";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(1272, 373);
+            label6.Name = "label6";
+            label6.Size = new Size(184, 20);
+            label6.TabIndex = 12;
+            label6.Text = "Sonuç Burada Görülecektir";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(1272, 447);
+            label7.Name = "label7";
+            label7.Size = new Size(184, 20);
+            label7.TabIndex = 13;
+            label7.Text = "Sonuç Burada Görülecektir";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(1272, 525);
+            label8.Name = "label8";
+            label8.Size = new Size(184, 20);
+            label8.TabIndex = 14;
+            label8.Text = "Sonuç Burada Görülecektir";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(978, 451);
+            label9.Name = "label9";
+            label9.Size = new Size(228, 20);
+            label9.TabIndex = 15;
+            label9.Text = "CodeWord CRC ile Data Birleşimi";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(1021, 525);
+            label10.Name = "label10";
+            label10.Size = new Size(146, 20);
+            label10.TabIndex = 16;
+            label10.Text = "Hata Kontrol Sonucu";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(180, 899);
+            button3.Name = "button3";
+            button3.Size = new Size(178, 73);
+            button3.TabIndex = 17;
+            button3.Text = "Geri Dönmek İçin Lütfen Butona Basınız";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click_1;
+            // 
             // CRC
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1902, 1033);
+            Controls.Add(button3);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(button2);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(divisorTextBox);
+            Controls.Add(dividendTextBox);
             Controls.Add(label1);
             Controls.Add(button1);
             Controls.Add(label2);
@@ -82,5 +215,17 @@
         private Label label2;
         private Button button1;
         private Label label1;
+        private TextBox dividendTextBox;
+        private TextBox divisorTextBox;
+        private Label label3;
+        private Label label4;
+        private Button button2;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Button button3;
     }
 }
--- Projemiz/CRC.cs
+++ Projemiz/CRC.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,146 @@ namespace Projemiz
         public CRC()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FormAnaSyf formAnaSyf = new FormAnaSyf();
+            formAnaSyf.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CRCcs_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Gelen karakterin '0' veya '1' olup olmadığını kontrol et
+            if (e.KeyChar != '0' && e.KeyChar != '1')
+            {
+                // Eğer '0' veya '1' değilse, olayı engelle
+                e.Handled = true;
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+        private bool IsCRCValid(string crc)
+        {
+            // CRC sonucunun tüm bitlerini kontrol et
+            foreach (char bit in crc)
+            {
+                if (bit != '0')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private string CalculateCRC(string data, string divisor)
+        {
+            int dataLength = data.Length;
+            int divisorLength = divisor.Length;
+
+            // Data'yı uzat
+            string extendedData = data.PadRight(dataLength + divisorLength - 1, '0');
+
+            char[] dataArray = extendedData.ToCharArray();
+            char[] divisorArray = divisor.ToCharArray();
+
+            for (int i = 0; i < dataLength; i++)
+            {
+                if (dataArray[i] == '1')
+                {
+                    for (int j = 0; j < divisorLength; j++)
+                    {
+                        dataArray[i + j] = dataArray[i + j] == divisorArray[j] ? '0' : '1';
+                    }
+                }
+            }
+
+            // CRC bitlerini al
+            string crcBits = new string(dataArray, dataLength, divisorLength - 1);
+
+            return crcBits;
+
+        }
+
+        private void button2_Click_2(object sender, EventArgs e)
+        {
+            string data = dividendTextBox.Text;
+            string divisor = divisorTextBox.Text;
+
+            // CRC işlemi
+            string crcResult = CalculateCRC(data, divisor);
+
+            // CRC bitlerini verinin sonuna ekle
+            string dataWithCRC = data + crcResult;
+
+            // Alıcı tarafından doğrulama
+            string receivedData = dataWithCRC; // Alıcı veriyi aldıktan sonra
+
+            string receivedCRCResult = CalculateCRC(receivedData, divisor);
+
+            // Sonucu ekranda göster
+            label6.Text = crcResult;
+            label7.Text = dataWithCRC;
+
+            if (IsCRCValid(receivedCRCResult))
+            {
+                label8.Text += "\nHata Kontrolü: Hatasız";
+            }
+            else
+            {
+                label8.Text += "\nHata Kontrolü: Hatalı";
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            // PDF dosyasının yolunu belirtin
+            string pdfDosyaYolu = "C:\\Users\\talha\\Downloads\\9.10.2023 Siber - 1.pdf"; // Gerçek dosya yolunu belirtin
+
+            // Dosyanın varlığını kontrol etmek isterseniz:
+            if (File.Exists(pdfDosyaYolu))
+            {
+                // Dosyayı MemoryStream'e yükle
+                byte[] pdfBytes = File.ReadAllBytes(pdfDosyaYolu);
+                MemoryStream memoryStream = new MemoryStream(pdfBytes);
+
+                // SaveFileDialog ile kullanıcıya nereye kaydetmek istediğini sorma
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "PDF Dosyaları (*.pdf)|*.pdf";
+                saveFileDialog.FileName = "YeniDosyaAdi.pdf"; // İndirilen dosyanın adını belirleyin
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Dosyayı kullanıcının belirttiği yere kaydet
+                    File.WriteAllBytes(saveFileDialog.FileName, memoryStream.ToArray());
+                    MessageBox.Show("PDF İndirme Başarılı!", "Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Belirtilen PDF dosyası bulunamadı.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            FormAnaSyf formAnaSyf = new FormAnaSyf();
+            formAnaSyf.Show();
+            this.Hide();
         }
     }
 }
