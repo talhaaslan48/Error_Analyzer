fe7b60e9b86f3cbb55f1aa22ce6cef567144e26b
Talha Aslan <talhabesiktas1@gmail.com>
2023-10-31 16:40:03 +0300

    crc

---
+++ Projemiz/CRC.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Projemiz
+{
+    partial class CRC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CRC));
+            label2 = new Label();
+            button1 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Rockwell", 48F, FontStyle.Italic, GraphicsUnit.Point);
+            label2.Location = new Point(554, 29);
+            label2.Name = "label2";
+            label2.Size = new Size(631, 89);
+            label2.TabIndex = 3;
+            label2.Text = "CRC Algoritması\r\n";
+            label2.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(1508, 143);
+            button1.Name = "button1";
+            button1.Size = new Size(295, 68);
+            button1.TabIndex = 4;
+            button1.Text = "PDF İndir";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(215, 132);
+            label1.Name = "label1";
+            label1.Size = new Size(1216, 100);
+            label1.TabIndex = 5;
+            label1.Text = resources.GetString("label1.Text");
+            // 
+            // CRC
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1902, 1033);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Name = "CRC";
+            Text = "CRC";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label2;
+        private Button button1;
+        private Label label1;
+    }
+}
