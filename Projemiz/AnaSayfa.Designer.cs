217682de511d24a642fe59039d2f1803eb5b2cc6
yreen <yaren.sari02@gmail.com>
2023-12-07 12:19:38 +0300

    hamming 7.4

--- Projemiz/AnaSayfa.Designer.cs
+++ Projemiz/AnaSayfa.Designer.cs
@@ -20,146 +20,150 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAnaSayfa));
-            panel1 = new Panel();
-            button7 = new Button();
-            button4 = new Button();
-            button5 = new Button();
-            button6 = new Button();
-            button3 = new Button();
-            button1 = new Button();
-            label1 = new Label();
-            label2 = new Label();
-            panel1.SuspendLayout();
-            SuspendLayout();
-            // 
-            // panel1
-            // 
-            panel1.Controls.Add(button7);
-            panel1.Controls.Add(button4);
-            panel1.Controls.Add(button5);
-            panel1.Controls.Add(button6);
-            panel1.Controls.Add(button3);
-            panel1.Controls.Add(button1);
-            panel1.Location = new Point(10, 9);
-            panel1.Margin = new Padding(3, 4, 3, 4);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(435, 1004);
-            panel1.TabIndex = 0;
-            // 
-            // button7
-            // 
-            button7.Location = new Point(25, 675);
-            button7.Margin = new Padding(3, 4, 3, 4);
-            button7.Name = "button7";
-            button7.Size = new Size(375, 51);
-            button7.TabIndex = 6;
-            button7.Text = "Read-Solomon Algoritması";
-            button7.UseVisualStyleBackColor = true;
-            button7.Click += button7_Click;
-            // 
-            // button4
-            // 
-            button4.Location = new Point(25, 401);
-            button4.Margin = new Padding(3, 4, 3, 4);
-            button4.Name = "button4";
-            button4.Size = new Size(375, 44);
-            button4.TabIndex = 5;
-            button4.Text = "CRC Algoritması";
-            button4.UseVisualStyleBackColor = true;
-            button4.Click += button4_Click;
-            // 
-            // button5
-            // 
-            button5.Location = new Point(25, 492);
-            button5.Margin = new Padding(3, 4, 3, 4);
-            button5.Name = "button5";
-            button5.Size = new Size(375, 45);
-            button5.TabIndex = 4;
-            button5.Text = "FEC Algoritması";
-            button5.UseVisualStyleBackColor = true;
-            button5.Click += button5_Click;
-            // 
-            // button6
-            // 
-            button6.Location = new Point(25, 581);
-            button6.Margin = new Padding(3, 4, 3, 4);
-            button6.Name = "button6";
-            button6.Size = new Size(375, 48);
-            button6.TabIndex = 3;
-            button6.Text = "VRC Algoritması";
-            button6.UseVisualStyleBackColor = true;
-            button6.Click += button6_Click;
-            // 
-            // button3
-            // 
-            button3.Location = new Point(25, 309);
-            button3.Margin = new Padding(3, 4, 3, 4);
-            button3.Name = "button3";
-            button3.Size = new Size(375, 44);
-            button3.TabIndex = 2;
-            button3.Text = "LRC Algoritması";
-            button3.UseVisualStyleBackColor = true;
-            button3.Click += button3_Click;
-            // 
-            // button1
-            // 
-            button1.Location = new Point(25, 224);
-            button1.Margin = new Padding(3, 4, 3, 4);
-            button1.Name = "button1";
-            button1.Size = new Size(375, 45);
-            button1.TabIndex = 0;
-            button1.Text = "Hamming Algoritmassı";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Rockwell", 48F, FontStyle.Italic, GraphicsUnit.Point);
-            label1.Location = new Point(833, 28);
-            label1.Name = "label1";
-            label1.Size = new Size(535, 89);
-            label1.TabIndex = 1;
-            label1.Text = "ErrorAnalyzer";
-            // 
-            // label2
-            // 
-            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(579, 377);
-            label2.Name = "label2";
-            label2.Size = new Size(1056, 391);
-            label2.TabIndex = 2;
-            label2.Text = resources.GetString("label2.Text");
-            // 
-            // FormAnaSayfa
-            // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1902, 1055);
-            Controls.Add(label2);
-            Controls.Add(label1);
-            Controls.Add(panel1);
-            Margin = new Padding(3, 4, 3, 4);
-            Name = "FormAnaSayfa";
-            Text = "Ana Sayfa";
-            WindowState = FormWindowState.Maximized;
-            panel1.ResumeLayout(false);
-            ResumeLayout(false);
-            PerformLayout();
-        }
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAnaSayfa));
+			panel1 = new Panel();
+			button7 = new Button();
+			button4 = new Button();
+			button5 = new Button();
+			button6 = new Button();
+			button3 = new Button();
+			button1 = new Button();
+			label1 = new Label();
+			label2 = new Label();
+			button2 = new Button();
+			panel1.SuspendLayout();
+			SuspendLayout();
+			// 
+			// panel1
+			// 
+			panel1.Controls.Add(button2);
+			panel1.Controls.Add(button7);
+			panel1.Controls.Add(button4);
+			panel1.Controls.Add(button5);
+			panel1.Controls.Add(button6);
+			panel1.Controls.Add(button3);
+			panel1.Controls.Add(button1);
+			panel1.Location = new Point(9, 7);
+			panel1.Name = "panel1";
+			panel1.Size = new Size(381, 753);
+			panel1.TabIndex = 0;
+			// 
+			// button7
+			// 
+			button7.Location = new Point(22, 506);
+			button7.Name = "button7";
+			button7.Size = new Size(328, 38);
+			button7.TabIndex = 6;
+			button7.Text = "Read-Solomon Algoritması";
+			button7.UseVisualStyleBackColor = true;
+			button7.Click += button7_Click;
+			// 
+			// button4
+			// 
+			button4.Location = new Point(22, 301);
+			button4.Name = "button4";
+			button4.Size = new Size(328, 33);
+			button4.TabIndex = 5;
+			button4.Text = "CRC Algoritması";
+			button4.UseVisualStyleBackColor = true;
+			button4.Click += button4_Click;
+			// 
+			// button5
+			// 
+			button5.Location = new Point(22, 369);
+			button5.Name = "button5";
+			button5.Size = new Size(328, 34);
+			button5.TabIndex = 4;
+			button5.Text = "FEC Algoritması";
+			button5.UseVisualStyleBackColor = true;
+			button5.Click += button5_Click;
+			// 
+			// button6
+			// 
+			button6.Location = new Point(22, 436);
+			button6.Name = "button6";
+			button6.Size = new Size(328, 36);
+			button6.TabIndex = 3;
+			button6.Text = "VRC Algoritması";
+			button6.UseVisualStyleBackColor = true;
+			button6.Click += button6_Click;
+			// 
+			// button3
+			// 
+			button3.Location = new Point(22, 232);
+			button3.Name = "button3";
+			button3.Size = new Size(328, 33);
+			button3.TabIndex = 2;
+			button3.Text = "LRC Algoritması";
+			button3.UseVisualStyleBackColor = true;
+			button3.Click += button3_Click;
+			// 
+			// button1
+			// 
+			button1.Location = new Point(22, 118);
+			button1.Name = "button1";
+			button1.Size = new Size(328, 34);
+			button1.TabIndex = 0;
+			button1.Text = "Hamming Algoritmassı";
+			button1.UseVisualStyleBackColor = true;
+			button1.Click += button1_Click;
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Font = new Font("Rockwell", 48F, FontStyle.Italic, GraphicsUnit.Point);
+			label1.Location = new Point(729, 21);
+			label1.Name = "label1";
+			label1.Size = new Size(426, 72);
+			label1.TabIndex = 1;
+			label1.Text = "ErrorAnalyzer";
+			// 
+			// label2
+			// 
+			label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+			label2.Location = new Point(507, 283);
+			label2.Name = "label2";
+			label2.Size = new Size(924, 293);
+			label2.TabIndex = 2;
+			label2.Text = resources.GetString("label2.Text");
+			// 
+			// button2
+			// 
+			button2.Location = new Point(22, 175);
+			button2.Name = "button2";
+			button2.Size = new Size(328, 34);
+			button2.TabIndex = 7;
+			button2.Text = "Hamming Algoritması 7.4";
+			button2.UseVisualStyleBackColor = true;
+			button2.Click += button2_Click;
+			// 
+			// FormAnaSayfa
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(1664, 791);
+			Controls.Add(label2);
+			Controls.Add(label1);
+			Controls.Add(panel1);
+			Name = "FormAnaSayfa";
+			Text = "Ana Sayfa";
+			WindowState = FormWindowState.Maximized;
+			panel1.ResumeLayout(false);
+			ResumeLayout(false);
+			PerformLayout();
+		}
 
-        #endregion
+		#endregion
 
-        private Panel panel1;
+		private Panel panel1;
         private Button button7;
         private Button button4;
         private Button button5;
@@ -168,5 +172,6 @@
         private Button button1;
         private Label label1;
         private Label label2;
-    }
+		private Button button2;
+	}
 }
--- Projemiz/AnaSayfa.cs
+++ Projemiz/AnaSayfa.cs
@@ -1,63 +1,70 @@
 namespace Projemiz
 {
-    public partial class FormAnaSayfa : Form
-    {
-        public FormAnaSayfa()
-        {
-            InitializeComponent();
-        }
+	public partial class FormAnaSayfa : Form
+	{
+		public FormAnaSayfa()
+		{
+			InitializeComponent();
+		}
 
 
 
 
-        private void FormAnaSayfa_Load(object sender, EventArgs e)
-        {
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-        }
+		private void FormAnaSayfa_Load(object sender, EventArgs e)
+		{
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+		}
 
 
 
-        private void button3_Click(object sender, EventArgs e)
-        {
+		private void button3_Click(object sender, EventArgs e)
+		{
 
-            LRCcs form3 = new LRCcs();
-            form3.Show();
-            this.Hide();
-        }
+			LRCcs form3 = new LRCcs();
+			form3.Show();
+			this.Hide();
+		}
 
-        private void button4_Click(object sender, EventArgs e)
-        {
-            CRC form4 = new CRC();
-            form4.Show();
-            this.Hide();
-        }
+		private void button4_Click(object sender, EventArgs e)
+		{
+			CRC form4 = new CRC();
+			form4.Show();
+			this.Hide();
+		}
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            SingleCharacterForm form1 = new SingleCharacterForm();
-            form1.Show();
-            this.Hide();
-        }
+		private void button1_Click(object sender, EventArgs e)
+		{
+			SingleCharacterForm form1 = new SingleCharacterForm();
+			form1.Show();
+			this.Hide();
+		}
 
-        private void button5_Click(object sender, EventArgs e)
-        {
-            FEC form5 = new FEC();
-            form5.Show();
-            this.Hide();
-        }
+		private void button5_Click(object sender, EventArgs e)
+		{
+			FEC form5 = new FEC();
+			form5.Show();
+			this.Hide();
+		}
 
-        private void button6_Click(object sender, EventArgs e)
-        {
-            VRC form6 = new VRC();
-            form6.Show();
-            this.Hide();
-        }
+		private void button6_Click(object sender, EventArgs e)
+		{
+			VRC form6 = new VRC();
+			form6.Show();
+			this.Hide();
+		}
 
-        private void button7_Click(object sender, EventArgs e)
-        {
-            Reed_Solomon form7 = new Reed_Solomon();
-            form7.Show();
-            this.Hide();
-        }
-    }
+		private void button7_Click(object sender, EventArgs e)
+		{
+			Reed_Solomon form7 = new Reed_Solomon();
+			form7.Show();
+			this.Hide();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			SingleCharacterForm7 form7 = new SingleCharacterForm7();
+			form7.Show();
+			this.Hide();
+		}
+	}
 }
